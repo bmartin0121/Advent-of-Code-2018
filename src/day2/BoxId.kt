f211f0e32fb5df66f4bf84d1e3c2072d725d5299
Martin Budai <Martin_Budai@epam.com>
2018-12-02 16:11:35 +0100

    Day 2

--- src/day2/BoxId.kt
+++ src/day2/BoxId.kt
@@ -4,15 +4,25 @@ class BoxId {
 
 	val id: String
 	val charCount: Map<Char, Int>
-	
+
 	constructor(id: String) {
 		this.id = id
 		this.charCount = id.asIterable()
 			.distinct()
-		    .map { char -> char `to` id.asIterable().count { char == it } }
-		    .toMap()
+			.map { char -> char `to` id.asIterable().count { char == it } }
+			.toMap()
 	}
-		
-	fun containsALetterExactTimes(times: Int): Boolean = charCount.containsValue(times)
 	
+	override fun toString(): String = id
+
+	fun containsALetterExactTimes(times: Int): Boolean = charCount.containsValue(times)
+
+	fun differentIndeces(other: BoxId): List<Int> {
+		val result = ArrayList<Int>()
+		id.forEachIndexed { index, char ->
+			if (other.id[index] != char) result.add(index)
+		}
+		return result
+	}
+
 }
---
+++ src/day2/OneDifferent.kt
@@ -0,0 +1,24 @@
+package day2
+
+import common.readLines
+
+/**
+ * Second part
+ */
+
+fun main(args: Array<String>) {
+	var boxIds = readLines("day2.txt").map { BoxId(it) }
+	val idAndDifferentIndex = boxIds.mapIndexed { index, box ->
+		boxIds.drop(index + 1)
+			.map { box `to` it.differentIndeces(box) }
+			.find { it.second.size == 1 }
+	}.find { it != null }
+	if (idAndDifferentIndex != null) {
+		val id = idAndDifferentIndex.first.id
+		val index = idAndDifferentIndex.second[0]
+		val commonLetters = id.removeRange(index, index + 1)
+		println(commonLetters)
+	} else {
+		println("There are no box ids that differ in only one character!")
+	}
+}
