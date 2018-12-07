c670cfef9a45da3a270a0fa2253921ba06e6c007
Martin Budai <Martin_Budai@epam.com>
2018-12-07 11:47:37 +0100

    Day 6

--- src/day6/ChronalCoordinates.kt
+++ src/day6/ChronalCoordinates.kt
@@ -3,8 +3,12 @@ package day6
 import common.readLines
 
 val COORDINATE_REGEX = """(\d+), (\d+)""".toRegex()
+const val MAXIMUM_TOTAL_DISTANCE = 10_000
 
 fun main(args: Array<String>) {
+    /*
+     * First part
+     */
     val coordinates = readCoordinates()
     val edges = edgeCoordinates(coordinates.values)
     val closest = mapOfClosestCoordinate(edges, coordinates)
@@ -13,13 +17,41 @@ fun main(args: Array<String>) {
         .map { name -> closest.count { it.value == name } }
         .max()
     println(largestArea)
+
+    /*
+     * Second part
+     */
+    val startPoint = coordinates.values.first()
+    val area = floodFill(startPoint, coordinates.values)
+    println(area.size)
+}
+
+private fun floodFill(start: Pair<Int, Int>, coordinates: Collection<Pair<Int, Int>>): Set<Pair<Int, Int>> {
+    var queue = listOf(start)
+    var area = setOf<Pair<Int, Int>>()
+    while (!queue.isEmpty()) {
+        val current = queue.last()
+        queue = queue.dropLast(1)
+        if (!area.contains(current) && current.totalDistanceFromCoordinates(coordinates) < MAXIMUM_TOTAL_DISTANCE) {
+            area = area.plus(current)
+            queue = queue.plus(
+                listOf(
+                    (current.first + 1) to current.second,
+                    current.first to (current.second + 1),
+                    (current.first - 1) to current.second,
+                    current.first to (current.second - 1)
+                )
+            )
+        }
+    }
+    return area
 }
 
 private fun readCoordinates(): Map<Int, Pair<Int, Int>> = readLines("day6.txt")
-        .map { COORDINATE_REGEX.find(it)!!.destructured }
-        .map { it.component1().toInt() to it.component2().toInt() }
-        .mapIndexed { index, coordinate -> index to coordinate }
-        .toMap()
+    .map { COORDINATE_REGEX.find(it)!!.destructured }
+    .map { it.component1().toInt() to it.component2().toInt() }
+    .mapIndexed { index, coordinate -> index to coordinate }
+    .toMap()
 
 private fun mapOfClosestCoordinate(edges: Edges, coordinates: Map<Int, Pair<Int, Int>>): Map<Pair<Int, Int>, Int> {
     val closest = HashMap<Pair<Int, Int>, Int>()
@@ -50,8 +82,11 @@ infix fun Pair<Int, Int>.manhattanDistance(other: Pair<Int, Int>): Int {
     return Math.abs(this.first - other.first) + Math.abs(this.second - other.second)
 }
 
-fun edgeCoordinates(coordinates: Collection<Pair<Int, Int>>): Edges {
+private fun edgeCoordinates(coordinates: Collection<Pair<Int, Int>>): Edges {
     return Edges(coordinates.minBy { it.first }!!.first to coordinates.maxBy { it.first }!!.first,
         coordinates.minBy { it.second }!!.second to coordinates.maxBy { it.second }!!.second
     )
 }
+
+private fun Pair<Int, Int>.totalDistanceFromCoordinates(coordinates: Collection<Pair<Int, Int>>): Int =
+    coordinates.sumBy { this manhattanDistance it }
