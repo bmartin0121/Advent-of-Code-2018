ee2f3d944a2fdc93585dd6c01cca27f0d44fab68
Martin Budai <Martin_Budai@epam.com>
2018-12-17 14:18:54 +0100

    Day 11

--- src/day11/ChronalCharge.kt
+++ src/day11/ChronalCharge.kt
@@ -1,15 +1,30 @@
 package day11
 
+const val GRID_SIZE = 300
 const val GRID_SERIAL_NUMBER = 1955
 
 fun main(args: Array<String>) {
-    val maxCoord = generateGrid(1..298, 1..298)
-        .maxBy { calculate3x3Power(it.first, it.second) }
-    println(maxCoord)
+    /**
+     * First part
+     */
+    val max3x3 = generateGrid(1..(GRID_SIZE - 2), 1..(GRID_SIZE - 2))
+        .maxBy{ calculateNxNPower(it.first, it.second, 3) }
+    println(max3x3)
+
+
+    /**
+     * Second part
+     */
+    val max = (1..300)
+        .map  { generateGrid(1..(GRID_SIZE - it + 1), 1..(GRID_SIZE - it + 1)) to it}
+        .parallelStream()
+        .map { pair -> pair.first.maxBy { coord -> (calculateNxNPower(coord.first, coord.second, pair.second - 1)) } to pair.second }
+        .max { o1, o2 ->  calculateNxNPower(o1.first!!.first, o1.first!!.second, o1.second - 1) - calculateNxNPower(o2.first!!.first, o2.first!!.second, o2.second - 1)}
+    println(max)
 }
 
-private fun calculate3x3Power(x: Int, y: Int): Int =
-    generateGrid(0..2, 0..2)
+private fun calculateNxNPower(x: Int, y: Int, n: Int): Int =
+    generateGrid(0..n, 0..n)
         .map { FuelCell(x + it.first, y + it.second) }
         .sumBy { it.powerLevel(GRID_SERIAL_NUMBER) }
 
